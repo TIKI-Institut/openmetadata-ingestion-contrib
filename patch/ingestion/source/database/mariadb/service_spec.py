b14060218bfd2a561829a703985f462370d862da
wobu <buchnerw@googlemail.com>
2025-05-06 13:46:20 +0200

    ZOLDPA2-30 renamed example connector implementation with _example postfix
    to better distinguish that they are only examples and not having real changes

--- patch/ingestion/source/database/mariadb/service_spec.py
+++ patch/ingestion/source/database/mariadb/service_spec.py
@@ -1,7 +1,7 @@
 from metadata.utils.service_spec.default import DefaultDatabaseSpec
 
 ServiceSpec = DefaultDatabaseSpec(
-    metadata_source_class="ingestion_contrib.mariadb.metadata.MariadbCustomSource",
+    metadata_source_class="ingestion_contrib.mariadb_example.metadata.MariadbCustomSource",
     lineage_source_class="metadata.ingestion.source.database.mariadb.lineage.MariadbLineageSource",
     profiler_class="metadata.profiler.interface.sqlalchemy.mariadb.profiler_interface.MariaDBProfilerInterface",
 )
